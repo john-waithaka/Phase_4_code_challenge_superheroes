f681052dd640b65fb6efc1f17320151025d0728e
John Warui Waithaka <johnwaruiwaithaka@gmail.com>
2024-09-13 10:03:36 +0300

    working

---
+++ server/models.py
@@ -0,0 +1,134 @@
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+# from sqlalchemy.orm import validates
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy_serializer import SerializerMixin
+
+# metadata = MetaData(naming_convention={
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# })
+
+# db = SQLAlchemy(metadata=metadata)
+
+
+# class Hero(db.Model, SerializerMixin):
+#     __tablename__ = 'heroes'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     super_name = db.Column(db.String)
+
+#     # add relationship
+
+#     # add serialization rules
+
+#     def __repr__(self):
+#         return f'<Hero {self.id}>'
+
+
+# class Power(db.Model, SerializerMixin):
+#     __tablename__ = 'powers'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     description = db.Column(db.String)
+
+#     # add relationship
+
+#     # add serialization rules
+
+#     # add validation
+
+#     def __repr__(self):
+#         return f'<Power {self.id}>'
+
+
+
+
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    super_name = db.Column(db.String, nullable=False)
+
+    # Relationship with HeroPower
+    hero_powers = db.relationship('HeroPower', back_populates='hero')
+
+    # Association proxy to directly access Power from Hero
+    powers = association_proxy('hero_powers', 'power')
+
+    # Serialization rules
+    serialize_rules = ('-hero_powers.hero',)
+
+    def __repr__(self):
+        return f'<Hero {self.id}>'
+
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+    # Relationship with HeroPower
+    hero_powers = db.relationship('HeroPower', back_populates='power')
+
+    # Association proxy to directly access Hero from Power
+    heroes = association_proxy('hero_powers', 'hero')
+
+    # Serialization rules
+    serialize_rules = ('-hero_powers.power',)
+
+    # Validation for description length
+    @validates('description')
+    def validate_description(self, key, value):
+        if len(value) < 20:
+            raise ValueError('Description must be at least 20 characters long')
+        return value
+
+    def __repr__(self):
+        return f'<Power {self.id}>'
+
+
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String, nullable=False)
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
+
+    # Relationships
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
+
+    # Serialization rules
+    serialize_only = ('id', 'strength', 'hero_id', 'power_id')
+
+    # Validation for strength
+    @validates('strength')
+    def validate_strength(self, key, value):
+        allowed_strengths = ['Weak', 'Average', 'Strong']
+        if value not in allowed_strengths:
+            raise ValueError(f'Strength must be one of {allowed_strengths}')
+        return value
+
+    def __repr__(self):
+        return f'<HeroPower {self.id}>'
